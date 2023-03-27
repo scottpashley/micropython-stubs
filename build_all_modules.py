dcd08e3de203f636317cb9fab7df00aa900f2625
Jos Verlinde <jos_verlinde@hotmail.com>
2023-03-27 16:46:42 +0200

    better list
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- build_all_modules.py
+++ build_all_modules.py
@@ -23,21 +23,22 @@ def main(output_file="all_modules.json", input_dir="publish"):
     for pkg_path in Path(input_dir).rglob("pyproject.toml"):
         if "stdlib" in str(pkg_path):
             continue
+        # if not "1_18" in str(pkg_path) and not "1_17" in str(pkg_path):
+        #     continue
         try:
             add_package(pkg_path,all_modules)
-
-
-
         except KeyError as e:
             continue
     with open("all_modules.json", "w") as f:
         json.dump(all_modules, f, indent=4)
 
-def add_package(pkg_path:Path, all_modules, port="", board=""):
+def add_package(pkg_path:Path, all_modules, port="", board="", pkg_version=""):
     with open(pkg_path, 'rb') as f:
+        # port , board  and pkg_version are optional and are only used for stdlib modules
+        # to keep these consistend with the port and board they are included in
         pyproject = tomllib.load(f)
         pkg_name = pyproject["tool"]["poetry"]["name"]
-        pkg_version = pyproject["tool"]["poetry"]["version"]
+        pkg_version = pkg_version or pyproject["tool"]["poetry"]["version"]
         mpy_version = Version(pkg_version).base_version
         modules = pyproject["tool"]["poetry"]["packages"]
         familiy =""
@@ -70,7 +71,7 @@ def add_package(pkg_path:Path, all_modules, port="", board=""):
             if dep.startswith("micropython-"):
                 dep_pkg_path = pkg_path.parent.parent/ dep / "pyproject.toml"
                 # add stdlib modules for this port & board
-                add_package(dep_pkg_path,all_modules, port , board)
+                add_package(dep_pkg_path,all_modules, port , board, pkg_version)
 
 if __name__ == "__main__":
     main()
